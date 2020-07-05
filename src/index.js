0eb22eab7c828a8b158f8c586c3e1d8fcada2c3a
Ravi Kanani <ravikanani@mail.com>
2020-07-04 21:12:05 -0400

    Implemented login and register functionality

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./components/App";
 import Login from "./components/Auth/Login";
 import Register from "./components/Auth/Register";
 import * as serviceWorker from "./serviceWorker";
-
+import "semantic-ui-css/semantic.min.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 const Root = () => (
